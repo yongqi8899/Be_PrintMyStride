8eb1f85cce963aab92711801aea370e485d57d5b
yongqi8899 <1243994415@qq.com>
2024-08-26 23:26:07 +0200

    add cors

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ var corsOptions = {
   }, 
   credentials: true 
 }
-app.use(cors( ));
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.static(join(import.meta.dirname, "uploads")))
 app.use("/auth", authRouter);
